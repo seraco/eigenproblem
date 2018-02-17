f5ddda1c1fd33330e00b03e0988aaa6b851dcdf3
seraco <sebastiaramon_1994@hotmail.com>
2018-02-17 10:59:41 +0100

    modify gitignore not to commit .m~ files

---
+++ Q2.m
@@ -0,0 +1,14 @@
+Ao = 2;
+rhoo = 3;
+Eo = 4;
+s1 = 0.1;
+s2 = 0.2;
+s3 = 0.3;
+L = 5;
+N = 4;
+
+mat = CMaterialProperties(Ao,Eo,rhoo,s1,s2,s3);
+ful = CFullMatrices(L,N,mat);
+
+M = ful.mass();
+K = ful.stiffness();
